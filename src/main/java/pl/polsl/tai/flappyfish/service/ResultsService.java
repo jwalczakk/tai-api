e735ccd783d4757b67fe53cae52534e77cdecb90
Jakub Walczak <jakub.walczak@softiq.pl>
2021-01-15 09:09:56 +0100

    Problematic loggers deleted

--- src/main/java/pl/polsl/tai/flappyfish/service/ResultsService.java
+++ src/main/java/pl/polsl/tai/flappyfish/service/ResultsService.java
@@ -1,7 +1,5 @@
 package pl.polsl.tai.flappyfish.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,78 +20,73 @@ import java.util.stream.Collectors;
 @Service
 public class ResultsService {
 
-    private final static Logger log = LoggerFactory.getLogger(ResultsService.class);
+  private final ResultsRepository resultsRepository;
 
-    private final ResultsRepository resultsRepository;
+  @Autowired
+  public ResultsService(final ResultsRepository resultsRepository) {
+    this.resultsRepository = resultsRepository;
+  }
 
-    @Autowired
-    public ResultsService(final ResultsRepository resultsRepository) {
-        this.resultsRepository = resultsRepository;
+  @PostConstruct
+  public void prepareData() {
+    for (int i = 1; i <= 10; i++) {
+      String username = (i % 2 == 0) ? "champion" : ("user" + i);
+      Result result = Result.builder()
+          .id((long) i)
+          .score((long) new Random().nextInt(1000))
+          .username(username)
+          .resultDate(Instant.now())
+          .build();
+      resultsRepository.save(result);
+    }
+  }
+
+  public void insertResult(ResultDto resultDto) {
+    Result result = mapDtoToModel(resultDto);
+    resultsRepository.save(result);
+  }
+
+  public List<ResultDto> getAllResults() {
+    return resultsRepository.findAll().stream().map(this::mapModelToDto).collect(Collectors.toList());
+  }
+
+  public List<ResultDto> getSpecifiedResults(Map<String, String> params) {
+    String sortOrder = "DESC";
+    if (params.containsKey("sort") && params.get("sort").equalsIgnoreCase("ASC")) {
+      sortOrder = "ASC";
     }
 
-    @PostConstruct
-    public void prepareData() {
-        for (int i = 1; i <= 10; i++) {
-            String username = (i % 2 == 0) ? "champion" : ("user" + i);
-            Result result = Result.builder()
-                    .id((long) i)
-                    .score((long) new Random().nextInt(1000))
-                    .username(username)
-                    .resultDate(Instant.now())
-                    .build();
-            resultsRepository.save(result);
-        }
+    int top = 10;
+    if (params.containsKey("top")) {
+      top = Integer.parseInt(params.get("top"));
     }
 
-    public void insertResult(ResultDto resultDto) {
-        log.info("Inserting new result... {}", resultDto);
-        Result result = mapDtoToModel(resultDto);
-        resultsRepository.save(result);
+    final Sort sortBy = Sort.by(sortOrder.equals("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC, "score");
+    Pageable sortedByPriceDescNameAsc =
+        PageRequest.of(0, top, sortBy);
+
+    Page<Result> all;
+    if (params.containsKey("username")) {
+      all = resultsRepository.findAllByUsername(sortedByPriceDescNameAsc, params.get("username"));
+    } else {
+      all = resultsRepository.findAll(sortedByPriceDescNameAsc);
     }
+    return all.get().map(this::mapModelToDto).collect(Collectors.toList());
+  }
 
-    public List<ResultDto> getAllResults() {
-        log.info("Getting all results...");
-        return resultsRepository.findAll().stream().map(this::mapModelToDto).collect(Collectors.toList());
-    }
+  private Result mapDtoToModel(ResultDto dto) {
+    return Result.builder()
+        .username(dto.getUsername())
+        .score(dto.getScore())
+        .resultDate(dto.getResultDate())
+        .build();
+  }
 
-    public List<ResultDto> getSpecifiedResults(Map<String, String> params) {
-        log.info("Getting specified results...");
-        String sortOrder = "DESC";
-        if (params.containsKey("sort") && params.get("sort").equalsIgnoreCase("ASC")) {
-            sortOrder = "ASC";
-        }
-
-        int top = 10;
-        if (params.containsKey("top")) {
-            top = Integer.parseInt(params.get("top"));
-        }
-
-        final Sort sortBy = Sort.by(sortOrder.equals("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC, "score");
-        Pageable sortedByPriceDescNameAsc =
-                PageRequest.of(0, top, sortBy);
-
-        Page<Result> all;
-        if (params.containsKey("username")) {
-            all = resultsRepository.findAllByUsername(sortedByPriceDescNameAsc, params.get("username"));
-        } else {
-            all = resultsRepository.findAll(sortedByPriceDescNameAsc);
-        }
-        return all.get().map(this::mapModelToDto).collect(Collectors.toList());
-    }
-
-    private Result mapDtoToModel(ResultDto dto) {
-        return Result.builder()
-                .username(dto.getUsername())
-                .score(dto.getScore())
-                .resultDate(dto.getResultDate())
-                .build();
-    }
-
-    private ResultDto mapModelToDto(Result result) {
-        return ResultDto.builder()
-                .username(result.getUsername())
-                .score(result.getScore())
-                .resultDate(result.getResultDate())
-                .build();
-    }
+  private ResultDto mapModelToDto(Result result) {
+    return ResultDto.builder()
+        .username(result.getUsername())
+        .score(result.getScore())
+        .resultDate(result.getResultDate())
+        .build();
+  }
 }
