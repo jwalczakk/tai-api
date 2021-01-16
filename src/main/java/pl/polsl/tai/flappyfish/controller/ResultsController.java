2e07f9272725346b2b616ab456a0e2fbf752fb23
Jakub Walczak <jakub.walczak@softiq.pl>
2021-01-16 11:48:13 +0100

    CrossOrigin annotations added

--- src/main/java/pl/polsl/tai/flappyfish/controller/ResultsController.java
+++ src/main/java/pl/polsl/tai/flappyfish/controller/ResultsController.java
@@ -1,35 +1,41 @@
 package pl.polsl.tai.flappyfish.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import pl.polsl.tai.flappyfish.dto.ResultDto;
-import pl.polsl.tai.flappyfish.service.ResultsService;
-
 import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.polsl.tai.flappyfish.dto.ResultDto;
+import pl.polsl.tai.flappyfish.service.ResultsService;
 
 @RestController
 public class ResultsController {
 
-    private final ResultsService resultsService;
+  private final ResultsService resultsService;
 
-    @Autowired
-    public ResultsController(final ResultsService resultsService) {
-        this.resultsService = resultsService;
-    }
+  @Autowired
+  public ResultsController(final ResultsService resultsService) {
+    this.resultsService = resultsService;
+  }
 
-    @PostMapping("/results")
-    public ResponseEntity<String> insertNewResult(@RequestBody ResultDto result) {
-        resultsService.insertResult(result);
-        return ResponseEntity.ok().build();
-    }
+  @CrossOrigin
+  @PostMapping("/results")
+  public ResponseEntity<String> insertNewResult(@RequestBody ResultDto result) {
+    resultsService.insertResult(result);
+    return ResponseEntity.ok().build();
+  }
 
-    @GetMapping("/results")
-    public ResponseEntity<List<ResultDto>> getAllResults(@RequestParam Map<String, String> params) {
-        if (params.isEmpty()) {
-            return ResponseEntity.ok(resultsService.getAllResults());
-        }
-        return ResponseEntity.ok(resultsService.getSpecifiedResults(params));
+  @CrossOrigin
+  @GetMapping("/results")
+  public ResponseEntity<List<ResultDto>> getAllResults(@RequestParam Map<String, String> params) {
+    if (params.isEmpty()) {
+      return ResponseEntity.ok(resultsService.getAllResults());
     }
+    return ResponseEntity.ok(resultsService.getSpecifiedResults(params));
+  }
 }
